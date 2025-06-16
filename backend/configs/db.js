9be662b11951b67fb1ce1a61fd6cdf9d3344edc9
Krishna <krishnasikheriya001@gmail.com>
2025-06-16 20:18:05 +0530

    Just Finished the backend of the project

---
+++ backend/configs/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+
+    const databaseName = 'quickblog';
+
+    try {
+        mongoose.connection.on('connected', () => {
+            console.log("MongoDB Connected");
+        } )
+        await mongoose.connect(`${process.env.MONGODB_URI}/${databaseName}`)
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export default connectDB;
--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className='flex justify-center items-center h-screen'>
-      <div className='animate-spin rounded-full h-16 w-16 border-5 border-t-gray-200 border-gray-700'>
+      <div className='animate-spin rounded-full h-20 w-20 border-5 border-t-primary border-gray-200'>
 
       </div>
     </div>
